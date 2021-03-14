b2724d7c29ceb53cac71f097be8b11b9aa8bd4fc
Ben Parmeter <nebhead77@gmail.com>
2021-03-14 11:38:10 -0700

    Modified control.py to dispense multiple pumps simultaneously
    As in the title, control.py has been modified to allow multiple pumps to run simultaneously, making dispensing much faster.

--- control.py
+++ control.py
@@ -11,9 +11,7 @@
 # *****************************************
 
 # Set this to False on Raspberry Pi Host ()
-prototype_mode = True 
-prototype_mode = False # Comment out this line to enable prototype mode
-
+prototype_mode = False
 # *****************************************
 # Imported Libraries
 # *****************************************
@@ -24,6 +22,7 @@ import os
 import json
 import datetime
 from common import *
+import threading
 
 if(prototype_mode == True):
 	# Prototype Modules for Test Host (i.e. PC based testing)
@@ -32,9 +31,23 @@ else:
 	# Actual Modules for RasPi
 	from platform_raspi import PumpControl # Library for reading the ADC device
 
+stop_threads = False # Allows cancelling of drink pour threads 
+
 # *****************************************
 # Supporting Functions
 # *****************************************
+
+def Pour(pump_number, waitTime, platform):
+	print(f' * Thread: {pump_number} Started for {waitTime} seconds.')
+	platform.ActivatePump(pump_number)
+	for x in range(waitTime): 
+		time.sleep(1)
+		global stop_threads 
+		if(stop_threads): 
+			print(f' * Thread: {pump_number} cancelling.')
+			break
+	platform.DeActivatePump(pump_number)
+	print(f' * Thread: {pump_number} finished.')
 
 def PourDrink(drink_name):
 	drink_db = ReadDrinkDB()
@@ -48,42 +61,57 @@ def PourDrink(drink_name):
 		total_runtime = 0
 		percent_progress = 0
 		current_count = 0
+		global stop_threads
 
 		# Initialize Platform Object
 		settings = ReadSettings()
 		platform = PumpControl(settings)
 
 		print(f'Starting to prepare {drink_name}')
-		# Cycle through each ingredient in the drink recipe to get the total runtime
+		# Cycle through each ingredient in the drink recipe to get the highest runtime, to get the total runtime
 		for drink_ingredient, pump_runtime in drink_db['drinks'][drink_name]['ingredients'].items():
-			total_runtime = total_runtime + int(pump_runtime * (settings['flowrate'] / 100))
+			#total_runtime = total_runtime + int(pump_runtime * (settings['flowrate'] / 100))
+			total_runtime = max(total_runtime, int(pump_runtime * (settings['flowrate'] / 100)))
 
 		print(f'Total Runtime: {total_runtime}')
 
-		# Cycle through each ingredient and dispense the beverage
+		# Cycle through each ingredient and create threads to dispense the beverage
+		pumpThreads = []
 		for drink_ingredient, pump_runtime in drink_db['drinks'][drink_name]['ingredients'].items():
-			if (status['control']['stop'] == 0):
-				pump_number = 'none'
-				for index, value in settings['inventory'].items():
-					if(value == drink_ingredient):
-						pump_number = index
-						print(f'Pump number = {index}')
-				platform.ActivatePump(pump_number)
-				calculated_pump_runtime = max(1, int((pump_runtime * (settings['flowrate'] / 100)))) 
-				print(f'Calculated Pump Runtime for {drink_ingredient} = {calculated_pump_runtime}')
-				for x in range(calculated_pump_runtime):
-					current_count += 1
-					status = ReadStatus()
-					if (status['control']['stop'] == 0):
-						percent_progress = int((current_count / total_runtime) * 100)
-						status['status']['progress'] = percent_progress
-						WriteStatus(status)
-						time.sleep(1)
-					else:
-						break
-				platform.DeActivatePump(pump_number)
-			else:
+			pump_number = 'none'
+			for index, value in settings['inventory'].items():
+				if(value == drink_ingredient):
+					pump_number = index
+					#print(f'Pump number = {index}')
+
+			# Init thread
+			calculated_pump_runtime = max(1, int((pump_runtime * (settings['flowrate'] / 100)))) 
+			#print(f'Calculated Pump Runtime for {drink_ingredient} = {calculated_pump_runtime}')
+			pump_t = threading.Thread(target=Pour, args=(pump_number,calculated_pump_runtime,platform))
+			pumpThreads.append(pump_t)
+		
+		# start the pump threads		
+		for thread in pumpThreads:
+			thread.start()
+
+		# Report Progress to WebUI
+		for x in range(total_runtime):
+			current_count += 1
+			status = ReadStatus()
+			if (status['control']['stop'] == 1):
+				stop_threads = True
+				time.sleep(2)
+				stop_threads = False
 				break
+			percent_progress = int((current_count / total_runtime) * 100)
+			status['status']['progress'] = percent_progress
+			WriteStatus(status) 
+			time.sleep(1)
+
+		# wait for threads to finish
+		for thread in pumpThreads:
+			thread.join()
+
 		print('Finished. Cleaning up status file.')
 		status['status']['active'] = 0
 		status['control']['start'] = 0
